ac2463fd5269d5e4a20ab3aecf25fffdb4b14146
Matt Savoia <matt@1-800-rad-dude.com>
2025-07-07 23:29:29 -0400

    Add IndexTest

---
+++ test/my_app_web/service_live/service_live_test.exs
@@ -0,0 +1,79 @@
+defmodule MyAppWeb.ServiceLive.IndexTest do
+  use MyAppWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  require Ash.Query
+
+  alias MyApp.Operations.{Service, Location}
+
+  setup do
+    locations =
+      [
+        %{name: "HQ"},
+        %{name: "Downtown Station"},
+        %{name: "Northeast Station"}
+      ]
+      |> Ash.bulk_create!(Location, :create, return_records?: true)
+      |> Map.get(:records)
+
+    %{locations: locations}
+  end
+
+  test "create a new service", %{conn: conn, locations: locations} do
+    [%{id: location_1_id}, %{id: location_2_id} | _rest] = locations
+    {:ok, new_service_view, html} = live(conn, "/services/new")
+
+    assert html =~ "New Service"
+
+    attrs = %{name: "Overhaul", locations: [location_1_id, location_2_id]}
+
+    assert new_service_view
+           |> form("#service-form", service: attrs)
+           |> render_submit()
+
+    assert %Service{
+             locations: [
+               %Location{id: ^location_1_id},
+               %Location{id: ^location_2_id}
+             ]
+           } =
+             Ash.Query.filter(Service, name == ^attrs.name)
+             |> Ash.read!(load: [:locations])
+             |> List.first()
+  end
+
+  test "edit a service", %{conn: conn, locations: locations} do
+    [%{id: location_1_id}, %{id: location_2_id}, %{id: location_3_id}] = locations
+
+    %Service{} =
+      service =
+      Ash.create!(Service, %{name: "Overhaul", locations: [location_1_id, location_2_id]})
+
+    {:ok, edit_service_view, html} = live(conn, "/services/#{service.id}/edit")
+
+    assert html =~ "Edit Service"
+
+    attrs = %{locations: [location_2_id, location_3_id]}
+
+    select =
+      edit_service_view
+      |> element("#service_locations")
+      |> render
+
+    assert select =~ "<option selected=\"selected\" value=\"#{location_1_id}\">"
+    assert select =~ "<option selected=\"selected\" value=\"#{location_2_id}\">"
+    assert select =~ "<option value=\"#{location_3_id}\""
+
+    assert edit_service_view
+           |> form("#service-form", service: attrs)
+           |> render_submit()
+
+    assert %Service{
+             locations: [
+               %Location{id: ^location_2_id},
+               %Location{id: ^location_3_id}
+             ]
+           } =
+             Ash.get!(Service, service.id, load: [:locations])
+  end
+end
